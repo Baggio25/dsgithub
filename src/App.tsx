5bc5bb1a9659b9b1b270e261826f91ba56f88b4e
Baggio25 <rodrigo.baggio.si@gmail.com>
2021-07-19 09:32:51 -0300

    First Commit / Clear Initial Project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <>
+            <h1>Desafio Github API</h1>
+            <h3>Bootcamp Spring React - DevSuperior</h3>
+        </>
+    );
+};
 
 export default App;
